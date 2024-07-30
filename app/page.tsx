a0ff3ad2bdd222ddadf739b4bc8116d0ee41667c
Junion <69495294+Jun1on@users.noreply.github.com>
2024-07-30 16:45:08 -0400

    revert back

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,27 @@
 import { getFrameMetadata } from '@coinbase/onchainkit/frame';
 import type { Metadata } from 'next';
+import { NEXT_PUBLIC_URL } from './config';
 
 const frameMetadata = getFrameMetadata({
   buttons: [
     {
-      label: 'Download Extension',
+      label: 'Story time',
     },
     {
-      action: 'link',
-      label: 'Learn More',
-      target: 'https://uniswap.org'
+      action: 'tx',
+      label: 'Send Base Sepolia',
+      target: `${NEXT_PUBLIC_URL}/api/tx`,
+      postUrl: `${NEXT_PUBLIC_URL}/api/tx-success`,
     },
   ],
-  image: '/ext.screenshot.png',
-  post_url: 'https://yourdomain.com/api/frame'
+  image: {
+    src: `${NEXT_PUBLIC_URL}/park-3.png`,
+    aspectRatio: '1:1',
+  },
+  input: {
+    text: 'Tell me a story',
+  },
+  postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
 });
 
 export const metadata: Metadata = {
